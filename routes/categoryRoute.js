d91d20e79ba8c63e9dad13e9951a89fbf25a76b5
unknown <Lenovo@EJAJULANSARI.EJAJULANSARI>
2024-07-11 11:08:11 +0530

    added parent name and slug in get by id route

--- routes/categoryRoute.js
+++ routes/categoryRoute.js
@@ -264,6 +264,7 @@ categoryRoute.get('/:id', async (req, res) => {
     try {
         const { id } = req.params;
         const category = await Category.findById(id).populate('children');
+        category = category.toObject();
         if (!category) {
             return res.status(404).send({ msg: 'Category not found' });
         }
