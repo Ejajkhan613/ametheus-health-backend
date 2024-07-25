fd31734fd8f0ebec81d82c786db25cdef2604b3d
unknown <Lenovo@EJAJULANSARI.EJAJULANSARI>
2024-07-25 17:42:16 +0530

    updated create-order api

--- routes/checkoutRoute.js
+++ routes/checkoutRoute.js
@@ -89,11 +89,7 @@ const createOrder = async (totalCartPrice, currency) => {
 router.post('/create-order', 
     verifyToken,
     async (req, res) => {
-        // Validation results
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(400).json({ errors: errors.array() });
-        }
+        
         console.log(req.body);
 
         try {
