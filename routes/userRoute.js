c87e750769e0c7c73e218b87928c8b4e6f323c3a
unknown <Lenovo@EJAJULANSARI.EJAJULANSARI>
2024-07-26 19:04:35 +0530

    added remove products manufacturerID or update it with new manufacturerID api

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -231,6 +231,7 @@ userRouter.patch('/', Limiter, userValidation, verifyToken, async (req, res) => 
     try {
         const userId = req.userDetail._id;
         const { name, gender, dateOfBirth = "", mobile } = req.body;
+        console.log(dateOfBirth);
 
         // Find the user by ID
         const user = await UserModel.findById(userId);
@@ -245,10 +246,10 @@ userRouter.patch('/', Limiter, userValidation, verifyToken, async (req, res) => 
 
         // Update user details
         const updateData = {};
-        if (!name) updateData.name = name;
-        if (!gender) updateData.gender = gender;
-        if (!dateOfBirth) updateData.dateOfBirth = dateOfBirth || "";
-        if (!mobile) updateData.mobile = mobile;
+        if (name !== undefined) updateData.name = name;
+        if (gender !== undefined) updateData.gender = gender;
+        if (dateOfBirth !== undefined) updateData.dateOfBirth = dateOfBirth;
+        if (mobile !== undefined) updateData.mobile = mobile;
 
         const updatedUser = await UserModel.findByIdAndUpdate(userId, updateData, { new: true });
 
