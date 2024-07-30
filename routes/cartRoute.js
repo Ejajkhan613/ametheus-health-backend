4627c26660f4729070ecf68f6496d76d57ca341e
unknown <Lenovo@EJAJULANSARI.EJAJULANSARI>
2024-07-30 16:54:07 +0530

    debugging error

--- routes/cartRoute.js
+++ routes/cartRoute.js
@@ -252,7 +252,7 @@ router.post('/batch-loggedin', verifyToken, async (req, res) => {
                     heightUnit: variant.heightUnit,
                 }
             };
-            console.log(payload)
+            console.log(payload);
             return payload;
         }));
 
