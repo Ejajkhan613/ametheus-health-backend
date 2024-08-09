f5176c12b5da67ffc999fb512f4b0a7abc0f21e9
unknown <Lenovo@EJAJULANSARI.EJAJULANSARI>
2024-08-09 12:03:21 +0530

    updated all admin api's

--- routes/categoryRoute.js
+++ routes/categoryRoute.js
@@ -726,9 +726,9 @@ categoryRoute.get('/admin/slug/:slug', async (req, res) => {
             return res.status(404).send({ msg: 'Category not found' });
         }
 
-        // const products = await ProductModel.find({ categoryID: { $in: [category._id] } }).sort({ 'title': 1 }).lean();
+        const products = await ProductModel.find({ categoryID: { $in: [category._id] } }).sort({ 'title': 1 }).lean();
 
-        // category.products = products;
+        category.products = products;
 
         if (category.parent) {
             const parentData = await Category.findById(category.parent);
