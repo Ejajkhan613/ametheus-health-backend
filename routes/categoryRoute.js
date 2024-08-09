52d3f21a803ac8209f8044971192c7412f0bfff8
unknown <Lenovo@EJAJULANSARI.EJAJULANSARI>
2024-08-09 14:57:53 +0530

    updated all admin api's

--- routes/categoryRoute.js
+++ routes/categoryRoute.js
@@ -619,6 +619,10 @@ categoryRoute.get('/admin/:id', async (req, res) => {
         }
         category = category.toObject();
 
+        const products = await ProductModel.find({ categoryID: { $in: [id] } }).lean();
+
+        category.products = products;
+
         if (category.parent) {
             const parentData = await Category.findById(category.parent);
             if (!parentData) {
@@ -645,7 +649,7 @@ categoryRoute.get('/slug/:slug', async (req, res) => {
             return res.status(404).send({ msg: 'Category not found' });
         }
 
-        const products = await ProductModel.find({ categoryID: { $in: [category._id] }, isVisible: true }).sort({'title': 1}).lean();
+        const products = await ProductModel.find({ categoryID: { $in: [category._id] }, isVisible: true }).lean();
 
         // Fetch exchange rate based on user's selected currency
         let exchangeRate = { rate: 1 };
